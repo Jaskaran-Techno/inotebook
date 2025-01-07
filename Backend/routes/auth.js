8b819e4fe583ec67f662ea3dbe8e75985aae1d97
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2025-01-07 16:04:48 +0530

    Adding Data Validation

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const Router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-Router.post('/', (res, req) => {
+Router.post('/', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], (res, req) => {
     console.log(req.body);
     const user = User(req.body);
     user.save();
